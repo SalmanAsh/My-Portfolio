08decd64fc4f6110370622b46dbafef686f6f456
h45123sa <salman.ashraf@student.manchester.ac.uk>
2025-08-03 14:00:33 +0100

    add favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,12 @@
 import Theme from '../styles/theme';
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" type="image/png" href="/images/fav.png" />
+      </Head>
       <Theme>
         <Component {...pageProps} />
       </Theme>
